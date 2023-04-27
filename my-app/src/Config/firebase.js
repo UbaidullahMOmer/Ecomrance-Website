d0a503f397264f86a7a06e643a9d1a5934d88582
Ubaidullah <researchfigma@gmail.com>
2023-04-27 14:20:39 +0500

    install firebase and config folder

---
+++ my-app/src/Config/firebase.js
@@ -0,0 +1,16 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import {getAuth} from "firebase/auth"
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDV2Abr00KCxfbimc96w9eCCMCJTVSn3zA",
+  authDomain: "ubaidullah-366405.firebaseapp.com",
+  projectId: "ubaidullah-366405",
+  storageBucket: "ubaidullah-366405.appspot.com",
+  messagingSenderId: "404048888067",
+  appId: "1:404048888067:web:f8aca81d2f88db3ec4a936",
+  measurementId: "G-5WNL00GS95"
+};
+
+const app = initializeApp(firebaseConfig);
+export const auth = getAuthJ(app);
